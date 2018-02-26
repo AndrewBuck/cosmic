d921d5cf7bef0488b95aa4d919d461e541abf596
Andrew Buck <andrew.r.buck@gmail.com>
2018-02-25 16:22:11 -0800

    Add more questions.

--- createQuestions.py
+++ createQuestions.py
@@ -25,6 +25,96 @@ worthwhile.
 
 #--------------------------------------------------------------------------------
 
+qImageProblems, created = Question.objects.get_or_create(
+    text = 'Are there any problems with the image?',
+    descriptionText = 'Look over the image as a whole and check any of the following problems if they are present in the image.',
+    titleText = 'Image Problems',
+    aboutType = 'Image',
+    priority = 100000,
+    previousVersion = None
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 0,
+    inputType = 'checkbox',
+    text = 'Uneven brightness',
+    descriptionText = 'Different portions of the image have very different image brightness levels, especially the background.',
+    keyToSet = 'unevenBrightness',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 1,
+    inputType = 'checkbox',
+    text = 'Over exposed',
+    descriptionText = 'All of the stars are very bright, showing no gradual dropoff in brightness as you move away from them.',
+    keyToSet = 'overExposed',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 2,
+    inputType = 'checkbox',
+    text = 'Bad lines',
+    descriptionText = 'More than a few scatterered bad lines, several large clusters of many bad lines near eachother.',
+    keyToSet = 'badLines',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 3,
+    inputType = 'checkbox',
+    text = 'Bad pixels',
+    descriptionText = 'More than a few scatterered bad pixels, several large clusters of many bad pixels near eachother.',
+    keyToSet = 'badPixels',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 4,
+    inputType = 'checkbox',
+    text = 'Not full frame',
+    descriptionText = 'The image data does not completely fill the image file.',
+    keyToSet = 'notFullFrame',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 5,
+    inputType = 'checkbox',
+    text = 'Not science',
+    descriptionText = 'The image is not science data (or calibration data), it is a picture of a person, a telescope, or anything else.',
+    keyToSet = 'notScience',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 6,
+    inputType = 'checkbox',
+    text = 'Spam',
+    descriptionText = 'The image is an advertisement, etc.',
+    keyToSet = 'spam',
+    valueToSet = 'yes'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qImageProblems,
+    index = 7,
+    inputType = 'checkbox',
+    text = 'xxx',
+    descriptionText = 'The image contains adult content, or otherwise obscene material that should not be shown to non-moderators.',
+    keyToSet = 'xxx',
+    valueToSet = 'yes'
+    )
+
+#--------------------------------------------------------------------------------
 #TODO: Maybe we want to combine all of dark/flat/bias into 'calibration' frame since you can't tell them apart by looking?
 qFrameType, created = Question.objects.get_or_create(
     text = 'What kind of image is this?',
@@ -105,11 +195,24 @@ r, created = QuestionResponse.objects.get_or_create(
     valueToSet = 'corrupt'
     )
 
+pcImageProblems, created = AnswerPrecondition.objects.get_or_create(
+    descriptionText = 'Only for science frames.',
+    firstQuestion = qImageProblems,
+    secondQuestion = qFrameType
+    )
+
+pccImageProblems, created = AnswerPreconditionCondition.objects.get_or_create(
+    answerPrecondition = pcImageProblems,
+    invert = True,
+    key = 'notScience|spam|xxx',
+    value = 'yes|yes|yes'
+    )
+
 #--------------------------------------------------------------------------------
 
 qObjectsPresent, created = Question.objects.get_or_create(
     text = 'What kinds of objects appear to be present in the image?',
-    descriptionText = 'Having images tagged with what is visible in them helps determine the limiting magnitude of the instrument used to take the image, as well as in locating images who could not be automatically plate solved.',
+    descriptionText = 'Having images tagged with what is visible in them helps determine the limiting magnitude of the instrument used to take the image, as well as in locating images which could not be automatically plate solved.',
     titleText = 'Objects Present',
     aboutType = 'Image',
     priority = 1000,
@@ -207,7 +310,7 @@ r, created = QuestionResponse.objects.get_or_create(
     )
 
 pcObjectsInFrameType, created = AnswerPrecondition.objects.get_or_create(
-    descriptionText = 'Only for light frames.',
+    descriptionText = 'Only for science frames.',
     firstQuestion = qFrameType,
     secondQuestion = qObjectsPresent
     )
@@ -277,7 +380,7 @@ pccPointSourcesInFrame, created = AnswerPreconditionCondition.objects.get_or_cre
 
 qGalaxyStructure, created = Question.objects.get_or_create(
     text = 'Can you see structure in any of the galaxies in the image?',
-    descriptionText = 'Galaxy structure means spiral arms, a central bar, or a prominent central bulge.',
+    descriptionText = 'Galaxy structure means any of: spiral arms, a central bar, a prominent central bulge, etc.',
     titleText = 'Galaxy Structure',
     aboutType = 'Image',
     priority = 15000,
@@ -369,6 +472,124 @@ pccStarsInFrameYes, created = AnswerPreconditionCondition.objects.get_or_create(
     invert = False,
     key = 'containsStars',
     value = 'yes'
+    )
+
+#--------------------------------------------------------------------------------
+
+qNumStars, created = Question.objects.get_or_create(
+    text = 'How many stars are visible in the image?',
+    descriptionText = 'Knowing roughly how many stars we should expect to find in an image helps us fine tune the star detection algorithms for each specific image.',
+    titleText = 'Number of Stars',
+    aboutType = 'Image',
+    priority =  1000,
+    previousVersion = None
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qNumStars,
+    index = 0,
+    inputType = 'radioButton',
+    text = 'None',
+    descriptionText = 'No stars at all.',
+    keyToSet = 'numStars',
+    valueToSet = '0'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qNumStars,
+    index = 1,
+    inputType = 'radioButton',
+    text = 'Only one',
+    descriptionText = 'Only a single star.',
+    keyToSet = 'numStars',
+    valueToSet = '1'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qNumStars,
+    index = 2,
+    inputType = 'radioButton',
+    text = 'A few',
+    descriptionText = 'A few stars, up to 10 or so.',
+    keyToSet = 'numStars',
+    valueToSet = 'few'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qNumStars,
+    index = 3,
+    inputType = 'radioButton',
+    text = 'Tens',
+    descriptionText = '10 to 50 or so, more than can easily be counted, but not hundreds.',
+    keyToSet = 'numStars',
+    valueToSet = 'tens'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qNumStars,
+    index = 4,
+    inputType = 'radioButton',
+    text = 'Hundreds',
+    descriptionText = 'A large image containing hundreds or many hundreds of stars.',
+    keyToSet = 'numStars',
+    valueToSet = 'hundreds'
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qNumStars,
+    index = 4,
+    inputType = 'radioButton',
+    text = 'Thousands',
+    descriptionText = 'A very large image covering a wide portion of the sky with many thousands of stars in total.',
+    keyToSet = 'numStars',
+    valueToSet = 'thousands'
+    )
+
+pcStarsInFrame2, created = AnswerPrecondition.objects.get_or_create(
+    descriptionText = 'If stars are present.',
+    firstQuestion = qObjectsPresent,
+    secondQuestion = qNumStars
+    )
+
+pccStarsInFrameYes2, created = AnswerPreconditionCondition.objects.get_or_create(
+    answerPrecondition = pcStarsInFrame2,
+    invert = False,
+    key = 'containsStars',
+    value = 'yes'
+    )
+
+#--------------------------------------------------------------------------------
+
+
+qOutlineDSO, created = Question.objects.get_or_create(
+    text = 'Future Question: Outline galaxies and mark structure, etc.',
+    descriptionText = 'This will be a javascript tool to outline galaxies, nebula, etc, in an image manually.',
+    titleText = 'Outline DSO',
+    aboutType = '',
+    priority = 1000,
+    previousVersion = None
+    )
+
+r, created = QuestionResponse.objects.get_or_create(
+    question = qOutlineDSO,
+    index = 0,
+    inputType = 'checkbox',
+    text = 'Ok',
+    descriptionText = 'This does nothing right now.',
+    keyToSet = 'nothing',
+    valueToSet = 'yes'
+    )
+
+pcStarsInFrame, created = AnswerPrecondition.objects.get_or_create(
+    descriptionText = 'Always.',
+    firstQuestion = qGalaxyStructure,
+    secondQuestion = qOutlineDSO
+    )
+
+pcStarsInFrame, created = AnswerPrecondition.objects.get_or_create(
+    descriptionText = 'Always.',
+    firstQuestion = qClustersPresent,
+    secondQuestion = qOutlineDSO
     )
 
 #--------------------------------------------------------------------------------
