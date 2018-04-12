29daf5eebd080e6b943b6a82e59a61f1e5f3df5a
Andrew Buck <andrew.r.buck@gmail.com>
2018-04-12 09:02:54 -0700

    Adjusting variables.

--- createVariables.py
+++ createVariables.py
@@ -7,10 +7,10 @@ django.setup()
 from cosmicapp.models import *
 
 CosmicVariable.setVariable('astrometryNetTimeout1', 'int', '30')
-CosmicVariable.setVariable('astrometryNetDepth1', 'string', '6,10,20')
+CosmicVariable.setVariable('astrometryNetDepth1', 'string', '8,12,20')
 
 CosmicVariable.setVariable('astrometryNetTimeout2', 'int', '120')
-CosmicVariable.setVariable('astrometryNetDepth2', 'string', '8,26,50')
+CosmicVariable.setVariable('astrometryNetDepth2', 'string', '12,20,25')
 
 CosmicVariable.setVariable('astrometryNetRadius', 'float', '10')
 
@@ -19,7 +19,7 @@ CosmicVariable.setVariable('daofindThreshold', 'float', '3.0')
 CosmicVariable.setVariable('starfindThreshold', 'float', '3.0')
 
 CosmicVariable.setVariable('histogramMaxBins', 'int', '255')
-CosmicVariable.setVariable('histogramRejectionExponent', 'float', '1000.0')
+CosmicVariable.setVariable('histogramRejectionExponent', 'float', '100.0')
 CosmicVariable.setVariable('histogramIgnoreLower', 'float', '.25')
 CosmicVariable.setVariable('histogramIgnoreUpper', 'float', '.25')
 
